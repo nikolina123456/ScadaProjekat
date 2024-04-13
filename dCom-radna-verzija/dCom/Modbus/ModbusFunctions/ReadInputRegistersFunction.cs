810ee96f50a9bf7abef8bb5be45ccb218bc16b1f
nikolina123456 <nikolinapetkovic79@gmail.com>
2024-04-13 22:00:48 +0200

    Read funkcije

---
+++ dCom-radna-verzija/dCom/Modbus/ModbusFunctions/ReadInputRegistersFunction.cs
@@ -0,0 +1,63 @@
+﻿using Common;
+using Modbus.FunctionParameters;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Reflection;
+
+namespace Modbus.ModbusFunctions
+{
+    /// <summary>
+    /// Class containing logic for parsing and packing modbus read input registers functions/requests.
+    /// </summary>
+    public class ReadInputRegistersFunction : ModbusFunction
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReadInputRegistersFunction"/> class.
+        /// </summary>
+        /// <param name="commandParameters">The modbus command parameters.</param>
+        public ReadInputRegistersFunction(ModbusCommandParameters commandParameters) : base(commandParameters)
+        {
+            CheckArguments(MethodBase.GetCurrentMethod(), typeof(ModbusReadCommandParameters));
+        }
+
+        /// <inheritdoc />
+        public override byte[] PackRequest()
+        {
+            byte[] request = new byte[12];
+            ModbusReadCommandParameters ModbusRead = this.CommandParameters as ModbusReadCommandParameters;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModbusRead.TransactionId)), 0, request, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModbusRead.ProtocolId)), 0, request, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModbusRead.Length)), 0, request, 4, 2);
+            request[6] = ModbusRead.UnitId;
+            request[7] = ModbusRead.FunctionCode;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModbusRead.StartAddress)), 0, request, 8, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModbusRead.Quantity)), 0, request, 10, 2);
+
+            return request;
+        }
+
+        /// <inheritdoc />
+        public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
+        {
+            ModbusReadCommandParameters ModbusRead = this.CommandParameters as ModbusReadCommandParameters;
+            Dictionary<Tuple<PointType, ushort>, ushort> dic = new Dictionary<Tuple<PointType, ushort>, ushort>();
+            ushort byte_count = response[8];
+            ushort value;
+
+            int byte02_start = 7;
+            int byte01_start = 8;
+            for (int i = 0; i < byte_count / 2; i++)
+            {
+                byte second_byte = response[byte02_start += 2];
+                byte first_byte = response[byte01_start += 2];
+
+                value = (ushort)(first_byte + (second_byte << 8));
+
+
+                dic.Add(new Tuple<PointType, ushort>(PointType.ANALOG_INPUT, (ushort)(ModbusRead.StartAddress + i)), value);
+            }
+            return dic;
+        }
+    }
+}
